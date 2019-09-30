e7c5f0d64757f83d14b45dea4234ede118b4efaf
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2019-09-30 09:42:00 +0200

    Added forgotten files with newton routines.

---
+++ include/newtonlib.h
@@ -0,0 +1,17 @@
+#ifndef _H_NEWTON // once-only header
+#define _H_NEWTON
+
+void mul_cpx(double* a_re, double* a_im,
+             double b_re, double b_im,
+             double c_re, double c_im);
+
+void newton1(double x_im, double x_re,
+             int* attractor, int* convergence);
+
+void newton2(double x_im, double x_re,
+             int* attractor, int* convergence);
+
+void newton3(double x_im, double x_re,
+             int* attractor, int* convergence);
+
+#endif
